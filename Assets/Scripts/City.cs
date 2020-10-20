0a15aaaa45e5bd33639983049cb892021c45872a
Hoxtober <hoxtber@gmail.com>
2020-10-20 16:50:49 +0200

    Empty project with imported TMP

---
+++ Assets/Scripts/City.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New City", menuName = "City")]
+public class City : ScriptableObject
+{
+    public string cityName;
+    public string description;
+    public int[] resources = new int[3];
+    public Image cityImage;
+    public Color cityColor;
+
+    public void Print()
+    {
+        
+    }
+}
