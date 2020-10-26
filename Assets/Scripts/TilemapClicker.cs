9f1e78a293c2567b339605aca374f5b04d498903
Hoxtober <hoxtber@gmail.com>
2020-10-26 23:15:17 +0100

    Tilemap navigation test

---
+++ Assets/Scripts/TilemapClicker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapClicker : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Tilemap map;
+    public Transform player;
+    
+    void Start()
+    {
+        map = GetComponent<Tilemap>();
+        mainCamera = Camera.main;
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector3Int clickCellPosition = map.WorldToCell(clickWorldPosition);
+
+            player.position = map.CellToWorld(clickCellPosition);
+
+            Debug.Log(player.transform.position);
+        }
+    }
+}
